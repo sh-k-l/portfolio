7f0b95382bacf3e84947c2685f6c32b457efa1b3
sh-k-l <shakil.chowdhury@yahoo.com>
2019-04-04 10:28:24 +0100

    Better favicon

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => (
   <div className="section" id="about">
-    <h2>About</h2>
+    <h2>About Me</h2>
     <div className="about-wrapper">
       <div className="placeholder-img"></div>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sodales finibus arcu non iaculis. Phasellus orci est, tincidunt quis tempus in, volutpat ac enim. Nulla facilisis efficitur neque non lobortis. Phasellus fermentum vitae augue vel dapibus. Aliquam blandit urna convallis facilisis suscipit. Nam in consectetur nulla, ac vehicula turpis. </p>
