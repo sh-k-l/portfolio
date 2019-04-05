4c30b5722083bb43e8c140c25c6def314662e424
sh-k-l <shakil.chowdhury@yahoo.com>
2019-04-05 10:38:59 +0100

    Move 200 to index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { render } from 'react-snapshot';
 
 import 'font-awesome/css/font-awesome.min.css'
